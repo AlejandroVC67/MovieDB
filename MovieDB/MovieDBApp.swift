92ee784faf9e540f04adf2de452691f26adaa699
Alejandro Villa <vcalejandro97@gmail.com>
2022-12-02 14:21:25 -0500

    First commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Alejandro Villa on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
